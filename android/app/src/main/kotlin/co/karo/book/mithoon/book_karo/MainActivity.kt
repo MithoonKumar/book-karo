c774da227040f48a4893a70063a461be95cf5a7d
MithoonKumar <kumarmithoon6@gmail.com>
2020-01-12 20:18:20 +0530

    Create basic project structure

---
+++ android/app/src/main/kotlin/co/karo/book/mithoon/book_karo/MainActivity.kt
@@ -0,0 +1,12 @@
+package co.karo.book.mithoon.book_karo
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
